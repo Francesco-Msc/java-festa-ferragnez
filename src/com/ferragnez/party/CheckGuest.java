934e9dd957b538b4e296acc718c4967a77d7f2aa
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-16 16:05:09 +0100

    added request to user input

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,8 +1,12 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
         String[] Guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome e premi invio...");
+        String yourName = input.nextLine();
     }
 }
